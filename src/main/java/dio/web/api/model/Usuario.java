522888c5595219ad22caab493ebb917cb11ab8f2
Dgley Ramos <dgleysilva@gmail.com>
2022-09-27 05:25:23 -0300

    Criando o model para Usuario

---
+++ src/main/java/dio/web/api/model/Usuario.java
@@ -0,0 +1,66 @@
+package dio.web.api.model;
+
+public class Usuario {
+    private Integer id;
+    private String login;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login=" + login + '/' +
+                ", password=" + password + '/' +
+                '}';
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
